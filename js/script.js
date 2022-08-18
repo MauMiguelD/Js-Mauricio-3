526dae565489e2fd2e58fa555ce6a6f739dba585
Mauricio Miguel <maumiguel.dzovoniarkiewicz@gmail.com>
2022-08-18 20:43:57 -0300

    adicionando primeiros scripts

--- js/script.js
+++ js/script.js
@@ -1,9 +1,52 @@
-function checarEmail(){
-    if( document.forms[0].email.value=="" 
-       || document.forms[0].email.value.indexOf('@')==-1 
-         || document.forms[0].email.value.indexOf('.')==-1 )
-        {
-          alert( "Por favor, informe um E-MAIL válido!" );
-          return false;
+let camposObrigatorios = document.querySelectorAll('input.obrigatorio');
+let camposNumericos = document.querySelectorAll('input.numero');
+
+for(let emFoco of camposObrigatorios) {
+    validaCampo(emFoco);
+}
+
+for(let emFoco of camposNumericos) {
+    validaCampo(emFoco);
+}
+
+// if( this.getAttribute('class').match(/erro/)) {
+//     return false;
+// }
+
+function validaCampo(elemento) {
+
+    elemento.addEventListener('focusout', function(event){
+
+        event.preventDefault();
+
+        if(this.value == ''){
+            document.querySelector('.mensagem').innerHTML = 'Verifique o preenchimento dos campos em vermelho'
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        } else {
+            document.querySelector('mensagem').innerHTML = '';
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
         }
-    }
+    });
+}
+
+function validaCampoNumerico(elemento) {
+
+    elemento.addEventListener('focusout', function(event){
+
+        event.preventDefault();
+
+        if(this.value.match(/\d*/)){
+            document.querySelector('.mensagem').innerHTML = 'Verifique o preenchimento dos campos em vermelho'
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        } else {
+            document.querySelector('mensagem').innerHTML = '';
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+        }
+    });
+}
